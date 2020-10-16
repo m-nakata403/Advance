bbad101c432305f58894e791cefacb35fd8ca15d
m-nakata403 <61781047+m-nakata403@users.noreply.github.com>
2020-10-16 12:52:53 +0900

    Create VendingMachine.rb

---
+++ VendingMachine.rb
@@ -0,0 +1,77 @@
+class SalesManager
+  attr_reader :net_sales
+ 
+  def initialize
+    @net_sales = 0
+  end
+ 
+  def update(product)
+    @net_sales += product.price
+  end
+ end
+
+class Beverage
+  @@tax = 0.1
+  attr_reader :name, :price
+ 
+  def initialize(args)
+    @name = args.fetch(:name, 'サイダー')
+    @price = args.fetch(:price, 120)
+  end
+ 
+  def price
+    @price + (@price * @@tax).floor
+  end
+ end
+ 
+ class Tabaco
+  @@tax = 0.6
+  attr_reader :name, :price, :tax
+ 
+  def initialize(args)
+    @name = args.fetch(:name, 'テックタバコ')
+    @price = args.fetch(:price, 300)
+  end
+ 
+  def price
+    @price + (@price * @@tax).floor
+  end
+ end
+ 
+ class VendingMachine
+  attr_reader :products
+ 
+  def initialize(products, input, sales_maneger)
+    @products = products
+    @input = input
+    @sales_maneger = sales_maneger
+  end
+ 
+  def transaction
+    display_anounce
+    product = take_order
+    sales_maneger.update(product)
+  end
+ 
+  private
+ 
+  attr_reader :products, :sales_maneger
+ 
+  def display_anounce
+    puts '購入したい商品を以下から選んで、金額を入力してください'
+    products.each_with_index do |product, i|
+      puts "[#{i}] 商品名：#{product.name} 税込み価格：#{product.price}円"
+    end
+  end
+ 
+  def take_order
+    product = @products[@input]
+    puts "#{product.name}が選択されました"
+    product
+  end
+ 
+  def calc_change(payment, price)
+    payment - price
+  end
+ end
+ 
